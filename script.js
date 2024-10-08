3a47ab644df9275058e422e13c40855a31595e49
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-10-08 15:43:38 +0300

    update regexes

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 import Calculator from "./src/Calculator.js";
 import Operation from "./src/Operation.js";
 
+const fst_number = document.querySelector('#current-first-number')
+const snd_number = document.querySelector('#current-second-number')
+
 document.addEventListener("keypress", (event) => {
   if (event.key.match(/\d/) != null) {
-    console.log('pressed digit', event.key);
+    console.log('pressed digit');
+    fst_number.innerHTML += event.key
+  } else if (event.key.match(/\.|,/) != null) {
+    console.log('pressed .');
   } else if (event.key.match(/\+/) != null) {
     console.log('pressed +');
   } else if (event.key.match(/-/) != null) {
@@ -12,6 +18,12 @@ document.addEventListener("keypress", (event) => {
     console.log('pressed ×');
   } else if (event.key.match(/\/|:/) != null) {
     console.log('pressed ÷');
+  } else if (event.key.match(/=/) != null) {
+    console.log('pressed =');
+  } else if (event.key.match(/c/) != null) {
+    console.log('pressed C');
+  } else if (event.key.match(/C|A/) != null) {
+    console.log('pressed AC');
   } 
 });
 
