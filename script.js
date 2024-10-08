4dc1a127c54c99b5c063934eff13b14f522f1586
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-10-08 15:28:30 +0300

    added regexes

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 import Calculator from "./src/Calculator.js";
 import Operation from "./src/Operation.js";
+
+document.addEventListener("keypress", (event) => {
+  if (event.key.match(/\d/) != null) {
+    console.log('pressed digit', event.key);
+  } else if (event.key.match(/\+/) != null) {
+    console.log('pressed +');
+  } else if (event.key.match(/-/) != null) {
+    console.log('pressed -');
+  } else if (event.key.match(/\*|x|X/) != null) {
+    console.log('pressed ×');
+  } else if (event.key.match(/\/|:/) != null) {
+    console.log('pressed ÷');
+  } 
+});
+
+document.querySelectorAll(".number-button").forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(element.innerHTML);
+  });
+});
